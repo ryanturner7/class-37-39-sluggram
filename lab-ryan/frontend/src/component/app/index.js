bb0ab676c0df2c73a54a3dd8aa3b7d68ae209d72
Ryan Turner <ryanthepit@gmail.com>
2017-08-03 18:27:17 -0700

    reducers, middleware and app component

--- lab-ryan/frontend/src/component/app/index.js
+++ lab-ryan/frontend/src/component/app/index.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import {BrowserRouter, Route, Link} from 'react-router-dom';
+import appStoreCreate from '../../lib/app-store-create.js';
 
+let store = appStoreCreate();
 
 class App extends React.Component {
   render(){
     return (
       <div className='app'>
-      <BrowserRouter>
-        <div>
-        <header>
-        <h1> Hello World! </h1>
-        </header>
-      </div>
-      </BrowserRouter>
+        <Provider store={store}>
+          <BrowserRouter>
+            <div>
+                <header>
+                  <h1> Hello World! </h1>
+                </header>
+              </div>
+          </BrowserRouter>
+        </Provider>
       </div>
     )
   }
-}
 
 export default App
